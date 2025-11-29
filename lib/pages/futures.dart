5a8c513bfc5dee11215e704fb87accf8f865710e
PushkarOnGit <pushkarjadhav9245@gmail.com>
2025-11-29 16:24:32 +0530

    just created required files and a init state

---
+++ lib/pages/futures.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+
+class Futures extends StatefulWidget {
+  const Futures({super.key});
+
+  @override
+  State<Futures> createState() => _FuturesState();
+}
+
+class _FuturesState extends State<Futures> {
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(body: Center(child: Text('Futures Explained')));
+  }
+}
