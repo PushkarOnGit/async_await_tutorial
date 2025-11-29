19123d5a7ca26daecea1ea70e97c2c6e2c267008
PushkarOnGit <pushkarjadhav9245@gmail.com>
2025-11-29 17:05:18 +0530

    setup to use delay

--- lib/pages/futures.dart
+++ lib/pages/futures.dart
@@ -11,46 +11,45 @@ class Futures extends StatefulWidget {
 class _FuturesState extends State<Futures> {
   /* 
   Can't make the init statement Async and Await as it will change the core type of it from 
-  Void to Future<datatype> void
+  void to Future<datatype> void
   */
   @override
   void initState() {
-    getStudentDetails();
+    // getStudentDetails();
     super.initState();
   }
 
-  Future<String> getStudentDetails() async {
-    const url = "https://jsonplaceholder.typicode.com/users";
-    // http
-    //     .get(Uri.parse(url))
-    //     .then((value) => print(value.body))
-    //     .catchError((error) => print(error));
+  // Future<String> getStudentDetails() async {
+  //   const url = "https://jsonplaceholder.typicode.com/users";
+  // http
+  //     .get(Uri.parse(url))
+  //     .then((value) => print(value.body))
+  //     .catchError((error) => print(error));
 
-    /*
+  /*
       This method is a bit ineffieicent 
       Causes a lot of nesting in function of then 
       making the code redablity and understand difficult
     */
 
-    /*
+  /*
     Used the async await to do the same thing but with improved redablity and less to no nesting 
     */
 
-    /*
+  /*
     Then used Future<datatype> fucntion name to return the value from the function with async and await
     */
 
-    late http.Response res;
+  //   late http.Response res;
 
-    try {
-      res = await http.get(Uri.parse(url));
-      print(res.body);
-    } catch (error) {
-      print(error.toString());
-    }
-
-    return res.body;
-  }
+  //   try {
+  //     res = await http.get(Uri.parse(url));
+  //     print(res.body);
+  //   } catch (error) {
+  //     print(error.toString());
+  //   }
+  //   return res.body;
+  // }
 
   @override
   Widget build(BuildContext context) {
